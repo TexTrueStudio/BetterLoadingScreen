8a3bd220c1db62b158cb1c47aadc5d78d7b8e893
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-09-24 13:54:15 +0800

    fix

--- quilt/src/main/java/me.shedaniel/betterloadingscreen/mixin/quilt/MixinMinecraft.java
+++ quilt/src/main/java/me.shedaniel/betterloadingscreen/mixin/quilt/MixinMinecraft.java
@@ -3,13 +3,15 @@ package me.shedaniel.betterloadingscreen.mixin.quilt;
 import dev.quantumfusion.taski.builtin.StageTask;
 import dev.quantumfusion.taski.builtin.StepTask;
 import me.shedaniel.betterloadingscreen.Tasks;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
+
+
 import net.minecraft.client.Minecraft;
+import org.quiltmc.loader.api.QuiltLoader;
+import org.quiltmc.loader.api.entrypoint.EntrypointContainer;
 import org.quiltmc.loader.impl.QuiltLoaderImpl;
 import org.quiltmc.loader.impl.util.ExceptionUtil;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,15 +32,15 @@ public abstract class MixinMinecraft {
         if (runDir == null) {
             runDir = new File(".");
         }
-        List<EntrypointContainer<ModInitializer>> commonContainers = FabricLoader.getInstance().getEntrypointContainers("main", ModInitializer.class);
-        List<EntrypointContainer<ClientModInitializer>> clientContainers = FabricLoader.getInstance().getEntrypointContainers("client", ClientModInitializer.class);
+        List<EntrypointContainer<ModInitializer>> commonContainers = QuiltLoader.getEntrypointContainers("main", ModInitializer.class);
+        List<EntrypointContainer<ClientModInitializer>> clientContainers = QuiltLoader.getEntrypointContainers("client", ClientModInitializer.class);
         
         QuiltLoaderImpl.INSTANCE.prepareModInit(runDir.toPath(), gameInstance);
         StepTask common = new StepTask("Common", commonContainers.size());
         StepTask client = new StepTask("Client", clientContainers.size());
-        Tasks.MAIN.setSubTask(new StageTask("Loading Mods", common, client));
-        invoke("main", commonContainers, common, ModInitializer::onInitialize);
-        invoke("client", clientContainers, client, ClientModInitializer::onInitializeClient);
+        Tasks.MAIN.setSubTask(new StageTask("Loading Mods", common));
+        //invoke("main", commonContainers, common, ModInitializer::onInitialize);
+        //invoke("client", clientContainers, client, ClientModInitializer::onInitializeClient);
         Tasks.MAIN.next();
     }
 
@@ -53,7 +55,7 @@ public abstract class MixinMinecraft {
                 exception = ExceptionUtil.gatherExceptions(t,
                         exception,
                         exc -> new RuntimeException(String.format("Could not execute entrypoint stage '%s' due to errors, provided by '%s'!",
-                                name, container.getProvider().getMetadata().getId()),
+                                name, container.getProvider().metadata().id()),
                                 exc));
             }
             
--- quilt/src/main/java/me.shedaniel/betterloadingscreen/quilt/BetterLoadingScreenQuiltPreInit.java
+++ quilt/src/main/java/me.shedaniel/betterloadingscreen/quilt/BetterLoadingScreenQuiltPreInit.java
@@ -3,15 +3,14 @@ package me.shedaniel.betterloadingscreen.quilt;
 import net.fabricmc.api.EnvType;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import org.quiltmc.loader.api.minecraft.MinecraftQuiltLoader;
 
 import java.lang.reflect.InvocationTargetException;
-
-import static org.quiltmc.loader.api.minecraft.MinecraftQuiltLoader.getEnvironmentType;
 
 public class BetterLoadingScreenQuiltPreInit implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch(ModContainer mod) {
-        if (getEnvironmentType() == EnvType.CLIENT) {
+        if (MinecraftQuiltLoader.getEnvironmentType() == EnvType.CLIENT) {
             try {
                 Class.forName("me.shedaniel.betterloadingscreen.quilt.BetterLoadingScreenQuiltPreInitClient")
                         .getDeclaredMethod("onPreLaunch")
