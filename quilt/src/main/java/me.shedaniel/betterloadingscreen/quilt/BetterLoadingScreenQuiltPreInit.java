cae20d9e41420d04a060426e1fcc8a7d8b9d1d76
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-11-11 23:01:33 +0800

    fix

--- quilt/src/main/java/me.shedaniel/betterloadingscreen/quilt/BetterLoadingScreenQuiltPreInit.java
+++ quilt/src/main/java/me.shedaniel/betterloadingscreen/quilt/BetterLoadingScreenQuiltPreInit.java
@@ -1,15 +1,16 @@
 package me.shedaniel.betterloadingscreen.quilt;
 
 import net.fabricmc.api.EnvType;
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+//import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.loader.api.minecraft.MinecraftQuiltLoader;
+//import org.quiltmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class BetterLoadingScreenQuiltPreInit implements PreLaunchEntrypoint {
     @Override
-    public void onPreLaunch(ModContainer mod) {
+    public void onPreLaunch() {
         if (MinecraftQuiltLoader.getEnvironmentType() == EnvType.CLIENT) {
             try {
                 Class.forName("me.shedaniel.betterloadingscreen.quilt.BetterLoadingScreenQuiltPreInitClient")
